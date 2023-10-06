138ff8042a3b95b0547ad1cc797ac3d8777a26c7
Daiki4529 <jonasbarth.lyceealain@gmail.com>
2023-10-06 17:46:45 +0200

    Simplification encore plus avancée des Middleware

--- server/server.js
+++ server/server.js
@@ -1,38 +1,10 @@
 const express = require("express");
 const app = express();
 
-/*
-app.get("/", (req, res, next) => {
-  const bufferList = [];
-  req.on("data", (chunch) => bufferList.push(chunch));
-  req.on("end", () => {
-    console.log(JSON.parse(Buffer.concat(bufferList).toString()));
-    res.send("GET request to the homepage");
-  });
-});
-
-app.post("/", function (req, res, next) {
-  const bufferList = [];
-  req.on("data", (chunch) => bufferList.push(chunch));
-  req.on("end", () => {
-    console.log(JSON.parse(Buffer.concat(bufferList).toString()));
-    res.send("POST request to the homepage");
-  });
-});
-
-app.put("/", function (req, res, next) {
-  const bufferList = [];
-  req.on("data", (chunch) => bufferList.push(chunch));
-  req.on("end", () => {
-    console.log(JSON.parse(Buffer.concat(bufferList).toString()));
-    res.send("PUT request to the homepage");
-  });
-});
-*/
-
 // Les 3 fonctions du cours 1 sont appelées des "Middleware" => fonctions qui prennent req/res/next
 // On peut tout simplifier en une seule fonction
 
+/*
 function parseBody(req, res, next) {
   const bufferList = [];
   req.on("data", (chunch) => bufferList.push(chunch));
@@ -62,7 +34,32 @@ app.put("/", parseBody, (req, res, next) => {
 app.get("/", (req, res, next) => {
   res.send("Hello world from GET");
 });
+*/
+
+// Encore une fois, le code précédent était inutile
+// On peut simplement utiliser la fonction suivante :
+
+app.use(express.json());
+
+// Le parse du body ne sert plus à rien
+// Ainsi :
+
+app.get("/", (req, res, next) => {
+  res.send("Hello world from GET");
+});
+
+app.post("/", (req, res, next) => {
+  res.send("Hello world from POST : " + JSON.stringify(req.body));
+});
+
+app.put("/", (req, res, next) => {
+  res.send("Hello world from PUT : " + JSON.stringify(req.body));
+});
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
+
+// Or sans expliquer à la méthode GET/POST/PUT quel type de body il va afficher,
+// il va simplement afficher {} dans le body
+// Voir [request.http] pour voir comment faire
